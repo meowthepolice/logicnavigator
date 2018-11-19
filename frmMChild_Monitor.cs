ad83b4415c9a1774e77c1f08c0dc6595b70ba16e
meowthepolice <43780604+meowthepolice@users.noreply.github.com>
2018-11-20 00:21:12 +1100

    Beginning of adding S2oE Support
    Added in a timer to check if new packets received on listened port.
    
    Currently the "Receive" button acts as a toggle.. which I should change to be visibly actively receiving/not receiving because it's not obvious.
    
    I tested this with my S2oE client I use for testing networking setups for westrace gear and it receives packets fine.
    
    I also added in the Send area the foundations for sending S2oE packets to Westrace.. I'm not sure this is needed functionality for Logic Navigator but is useful for debugging purposes during developement.

--- frmMChild_Monitor.cs
+++ frmMChild_Monitor.cs
@@ -8,7 +8,7 @@ using System.Globalization;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-
+using System.Timers;
 
 namespace Logic_Navigator
 {
@@ -30,10 +30,73 @@ namespace Logic_Navigator
         public bool recentChange = false;
         public bool block = false;
         public string searchString = "";
+        bool rx_running = false;
+        System.Timers.Timer aTimer = new System.Timers.Timer();
+
+        Crc16 crc_generator = new Crc16();
+        UdpClient listener = new UdpClient();
 
         private int X;
         private int Y;
         private bool Paused = false;
+
+        // Some C# for producing CRC-16 for X-25 from https://stackoverflow.com/questions/22860356/how-to-generate-a-crc-16-from-c-sharp
+        static byte[] HexToBytes(string input)
+        {
+            byte[] result = new byte[input.Length / 2];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = Convert.ToByte(input.Substring(2 * i, 2), 16);
+            }
+            return result;
+        }
+        
+        public class Crc16
+        {
+            readonly ushort[] table = new ushort[256];
+
+            public int ComputeChecksum(params byte[] bytes)
+            {
+                ushort crc = 0xffff;
+                for (int i = 0; i < bytes.Length; ++i)
+                {
+                    byte index = (byte)(crc ^ bytes[i]);
+                    crc = (ushort)((crc >> 8) ^ table[index]);
+                }
+                return crc ^ 0xffff;
+            }
+
+            public byte[] ComputeChecksumBytes(params byte[] bytes)
+            {
+                int crc = ComputeChecksum(bytes);
+                return BitConverter.GetBytes(crc);
+            }
+
+            public Crc16()
+            {
+                ushort polynomial = (ushort)0x8408;
+                ushort value;
+                ushort temp;
+                for (ushort i = 0; i < table.Length; ++i)
+                {
+                    value = 0;
+                    temp = i;
+                    for (byte j = 0; j < 8; ++j)
+                    {
+                        if (((value ^ temp) & 0x0001) != 0)
+                        {
+                            value = (ushort)((value >> 1) ^ polynomial);
+                        }
+                        else
+                        {
+                            value >>= 1;
+                        }
+                        temp >>= 1;
+                    }
+                    table[i] = value;
+                }
+            }
+        }
 
         public frmMChild_Monitor(ArrayList interlockingOldPointer, ArrayList interlockingNewPointer, Font drawFont)
         {
@@ -88,58 +151,86 @@ namespace Logic_Navigator
         public void SendDemonstrateUdpClientBug()
         {
             IPAddress send_to_address = IPAddress.Parse(IPAddressText.Text);
-            listenerIP = new IPEndPoint(send_to_address, 4201);
-            //Console.WriteLine(); Console.WriteLine("UdpClient.Receive Bug");
+            IPAddress srcAddress = IPAddress.Parse(srcIPAddressText.Text);
+            int srcPort = int.Parse(srcPortText.Text);
+            IPEndPoint destIP = new IPEndPoint(send_to_address, srcPort);
+            IPEndPoint srcIP = new IPEndPoint(srcAddress, srcPort);
+            Crc16 crc_generator = new Crc16();
 
-            // Setup listener socket
-            //UdpClient listener = new UdpClient(listenerIP); // Setup sending socket
-            UdpClient sender = new UdpClient();
-            sender.Connect(listenerIP);
+            byte[] bits_to_transmit = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-            // Sending  three datagrams to the listener 
-            Send(sender, "One");
-            Send(sender, "Two");
-            Send(sender, "Three");
-            Send(sender, "Four");
+            byte[] controls_payload = Concat(new byte[] { 0x81, 0x00 }, bits_to_transmit);
+            controls_payload = Concat(controls_payload, crc_generator.ComputeChecksumBytes(controls_payload));
 
-            // Now receive the three datagrams from the listener
-            //Receive(listener);
-            //Receive(listener);
-            //Receive(listener);
-           // Receive(listener);
+            byte[] payload = Concat(new byte[] { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x7e }, controls_payload);
 
-            //listener.Close();
-            sender.Close();
-        }
+            UdpClient listener = new UdpClient(srcIP); // Setup sending socket
 
+            listener.Connect(destIP);
 
-        public void ReceiveDemonstrateUdpClientBug()
-        {
-            IPAddress send_to_address = IPAddress.Parse(IPAddressText.Text);
-            listenerIP = new IPEndPoint(send_to_address, 4201);
-            //Console.WriteLine(); Console.WriteLine("UdpClient.Receive Bug");
+            SendBytes(listener, payload);
 
-            // Setup listener socket
-            UdpClient listener = new UdpClient(listenerIP); // Setup sending socket
-            //UdpClient sender = new UdpClient();
-            //sender.Connect(listenerIP);
-
-            // Sending  three datagrams to the listener 
-            //Send(sender, "One");
-            //Send(sender, "Two");
-            //Send(sender, "Three");
-            //Send(sender, "Four");
-
-            // Now receive the three datagrams from the listener
-            Receive(listener);
-            Receive(listener);
-            Receive(listener);
             Receive(listener);
 
             listener.Close();
-           // sender.Close();
         }
 
+        static byte[] Concat(byte[] a, byte[] b)
+        {
+            byte[] output = new byte[a.Length + b.Length];
+            for (int i = 0; i < a.Length; i++)
+                output[i] = a[i];
+            for (int j = 0; j < b.Length; j++)
+                output[a.Length + j] = b[j];
+            return output;
+        }
+
+        public void ReceiveDemonstrateUdpClientBug()
+        {
+            if (rx_running)
+            {
+                rx_running = false;
+                aTimer.Enabled = false;
+                listener.Close();
+            } else
+            {
+                IPAddress send_to_address = IPAddress.Parse(IPAddressText.Text);
+                IPAddress srcAddress = IPAddress.Parse(srcIPAddressText.Text);
+                int srcPort = int.Parse(srcPortText.Text);
+                IPEndPoint destIP = new IPEndPoint(send_to_address, srcPort);
+                IPEndPoint srcIP = new IPEndPoint(srcAddress, srcPort);
+                Crc16 test = new Crc16();
+
+                listener = new UdpClient(srcIP); // Setup sending socket
+
+                rx_running = true;
+                aTimer.Elapsed += new ElapsedEventHandler(ReceiveTimer);
+                aTimer.Interval = 500;
+                aTimer.Enabled = true;
+            }
+        }
+
+        public void ReceiveTimer(object source, ElapsedEventArgs e)
+        {
+            try
+            {
+                if (listener.Available > 0) // Only read if we have some data 
+                {                           // queued in the network buffer. 
+                    Receive(listener);
+                }
+            }
+            catch (Exception err)
+            {
+                Console.Write(err);
+            }
+        }
+
+        void SendBytes(UdpClient sender, byte[] s)
+        {
+            Console.WriteLine("Sending '" + s + "' (" + s.Length.ToString()
+                               + " bytes)");
+            sender.Send(s, s.Length);
+        }
 
         void Send(UdpClient sender, string s)
         {
@@ -154,12 +245,12 @@ namespace Logic_Navigator
             IPEndPoint from = new IPEndPoint(IPAddress.Any, 0);
             byte[] dgram = listener.Receive(ref from);
             string s = Encoding.ASCII.GetString(dgram, 0, dgram.Length);
-            COS.Text += s;
-            //Console.WriteLine
-            //(
-            //  "Received {0} bytes, s = '{1}', s.Length = {2}",
-            //  dgram.Length, s, s.Length
-            //);
+            //COS.Text += s;
+            Console.WriteLine
+            (
+              "Received {0} bytes, s = '{1}', s.Length = {2}",
+              dgram.Length, s, s.Length
+            );
         }
 
 
@@ -238,6 +329,13 @@ namespace Logic_Navigator
             this.SoundcheckBox = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.srcIPAddressText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.srcPortText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -251,11 +349,11 @@ namespace Logic_Navigator
             // 
             // IPAddressText
             // 
-            this.IPAddressText.Location = new System.Drawing.Point(12, 4);
+            this.IPAddressText.Location = new System.Drawing.Point(129, 5);
             this.IPAddressText.Name = "IPAddressText";
             this.IPAddressText.Size = new System.Drawing.Size(126, 20);
             this.IPAddressText.TabIndex = 30;
-            this.IPAddressText.Text = "192.168.0.17";
+            this.IPAddressText.Text = "172.30.5.65";
             this.IPAddressText.TextChanged += new System.EventHandler(this.searchString1_TextChanged_1);
             // 
             // COS
@@ -263,18 +361,18 @@ namespace Logic_Navigator
             this.COS.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.COS.Location = new System.Drawing.Point(-2, 85);
+            this.COS.Location = new System.Drawing.Point(-2, 208);
             this.COS.Multiline = true;
             this.COS.Name = "COS";
             this.COS.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.COS.Size = new System.Drawing.Size(390, 527);
+            this.COS.Size = new System.Drawing.Size(390, 404);
             this.COS.TabIndex = 31;
             // 
             // Pause
             // 
-            this.Pause.Location = new System.Drawing.Point(144, 1);
+            this.Pause.Location = new System.Drawing.Point(261, 65);
             this.Pause.Name = "Pause";
-            this.Pause.Size = new System.Drawing.Size(111, 40);
+            this.Pause.Size = new System.Drawing.Size(127, 37);
             this.Pause.TabIndex = 32;
             this.Pause.Text = "Loopback UDP Packets";
             this.Pause.UseVisualStyleBackColor = true;
@@ -283,7 +381,7 @@ namespace Logic_Navigator
             // SoundcheckBox
             // 
             this.SoundcheckBox.AutoSize = true;
-            this.SoundcheckBox.Location = new System.Drawing.Point(81, 42);
+            this.SoundcheckBox.Location = new System.Drawing.Point(12, 165);
             this.SoundcheckBox.Name = "SoundcheckBox";
             this.SoundcheckBox.Size = new System.Drawing.Size(57, 17);
             this.SoundcheckBox.TabIndex = 33;
@@ -310,11 +408,80 @@ namespace Logic_Navigator
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
+            // srcIPAddressText
+            // 
+            this.srcIPAddressText.Location = new System.Drawing.Point(129, 30);
+            this.srcIPAddressText.Name = "srcIPAddressText";
+            this.srcIPAddressText.Size = new System.Drawing.Size(126, 20);
+            this.srcIPAddressText.TabIndex = 36;
+            this.srcIPAddressText.Text = "172.30.5.66";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Interlocking Addr:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Our Adaptor Addr:";
+            // 
+            // srcPortText
+            // 
+            this.srcPortText.Location = new System.Drawing.Point(129, 56);
+            this.srcPortText.Name = "srcPortText";
+            this.srcPortText.Size = new System.Drawing.Size(126, 20);
+            this.srcPortText.TabIndex = 39;
+            this.srcPortText.Text = "3800";
+            this.srcPortText.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Port Number:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Sequence Number:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.TabIndex = 41;
+            this.textBox1.Text = "0";
+            // 
             // frmMChild_Monitor
             // 
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(388, 615);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.srcPortText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.srcIPAddressText);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.SoundcheckBox);
@@ -631,5 +798,25 @@ namespace Logic_Navigator
         {
             ReceiveDemonstrateUdpClientBug();
         }
+
+        public TextBox srcIPAddressText;
+        private Label label1;
+        private Label label2;
+        public TextBox srcPortText;
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private Label label3;
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private Label label4;
+        public TextBox textBox1;
     }
 }
